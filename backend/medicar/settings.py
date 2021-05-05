af3b6084f73dd97af5f8700e7265e16069c5ba7d
Caike Da Silva Reis <45696077+caikesilva@users.noreply.github.com>
2021-05-05 20:44:56 -0300

    alterando admin

--- backend/medicar/settings.py
+++ backend/medicar/settings.py
@@ -153,4 +153,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
--- backend/medicar/urls.py
+++ backend/medicar/urls.py
@@ -20,3 +20,22 @@ urlpatterns = [
     path('accounts/login/', LoginView.as_view()),
     path('accounts/registration/', RegisterView.as_view()),
 ]
+
+
+#Personalizando admin
+
+from django.contrib import admin
+from django.contrib.sites.models import Site
+from django.contrib.auth.models import User, Group
+from allauth.account.models import EmailAddress
+from rest_framework.authtoken.models import TokenProxy
+
+admin.site.site_header = 'Medicar'
+admin.site.index_title = 'Medicar API'
+admin.site.site_title = 'Medicar'
+
+admin.site.unregister(Site)
+admin.site.unregister(User)
+admin.site.unregister(Group)
+admin.site.unregister(TokenProxy)
+admin.site.unregister(EmailAddress)
