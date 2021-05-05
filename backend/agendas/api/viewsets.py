3fd17cc64baa2d4f890797152251e2bd6a9d8a8d
Caike Da Silva Reis <45696077+caikesilva@users.noreply.github.com>
2021-05-04 23:18:42 -0300

    configurando

--- backend/agendas/api/viewsets.py
+++ backend/agendas/api/viewsets.py
@@ -6,19 +6,19 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 
 class AgendaViewSet(ModelViewSet):
-
     serializer_class = AgendaSerializer
     permission_classes = [IsAuthenticated]
     authentication_classes = [TokenAuthentication]
-    http_method_names = ['get']
+    http_method_names = ['get']    
 
     def get_queryset(self):
-        queryset = Agenda.objects.exclude(dia__lte=date.today()).order_by('dia')
+        
+        queryset = Agenda.objects.filter(dia__gte=date.today()).order_by('dia')
         data_inicio = self.request.query_params.get('data_inicio', None)
         data_final = self.request.query_params.get('data_final', None)
         medico = self.request.query_params.get('medico', None)
         especialidade = self.request.query_params.get('especialidade', None)
-
+        
         if medico:
             queryset = queryset.filter(medico=medico)
         
@@ -28,4 +28,4 @@ class AgendaViewSet(ModelViewSet):
         if data_inicio and data_final:
             queryset = queryset.filter(dia__range=(data_inicio,data_final))
 
-        return queryset  
+        return queryset
